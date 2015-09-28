3ca59161ce8976e765c3d2330cd053511789f7f1
majun <jun.mash@ele.me>
2015-09-28 20:46:27 +0800

    submit spring-aop

---
+++ spring-aop/src/main/java/com/mj/spring/aop/aspect/AOPLog4jAnnotation.java
@@ -0,0 +1,12 @@
+package com.mj.spring.aop.aspect;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created by majun on 15/9/25.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AOPLog4jAnnotation {
+
+}
